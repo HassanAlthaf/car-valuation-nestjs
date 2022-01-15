ddd98ff99384a77bd0ce77105f8955e197589e07
HassanAlthaf <online@hassanalthaf.com>
2022-01-15 09:49:27 +0530

    feat (Reports): Create Report with User Association

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,21 @@
+import { Expose, Transform } from 'class-transformer';
+import { User } from '../../users/user.entity';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+  @Expose()
+  price: number;
+  @Expose()
+  year: number;
+  @Expose()
+  make: string;
+  @Expose()
+  model: string;
+  @Expose()
+  mileage: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
